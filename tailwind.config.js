07ffa31ae535f0c83eae11748b4f120e8a8597f2
Joseph Martin <se.josephmartin@gmail.com>
2023-11-08 13:58:47 +0800

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.html"],
+  content: ["./*.html", "./public/*.html"],
   theme: {
     colors: {
       color1: "#29c1a2",
