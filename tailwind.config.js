5bc0730377026629547b615868905243fb7d8053
Joseph Martin <se.josephmartin@gmail.com>
2023-11-08 12:13:34 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    colors: {
+      color1: "#29c1a2",
+      color2: "#FFFFFF",
+      color3: "#0077B6",
+    },
+    fontFamily: {
+      bebas: ["Bebas Neue", "sans-serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
